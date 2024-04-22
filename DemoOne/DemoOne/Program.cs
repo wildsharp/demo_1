85b0d31d9881c0042369efa322c6b6f5e911b1b6
Kristian Schmidt-Høier <k.schmidthoier@gmail.com>
2024-04-22 12:22:33 +0200

    basic functionality in place

--- DemoOne/DemoOne/Program.cs
+++ DemoOne/DemoOne/Program.cs
@@ -1,10 +1,13 @@
 using DemoOne.Components;
+using DemoOne.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IRegistrationHandler, RegistrationHandler>();
 
 var app = builder.Build();
 
